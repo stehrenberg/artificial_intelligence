21f1ae83fe0b3e394bb1238b6d984b3ec17c9710
Inyrie <inyrie@web.de>
2015-05-17 16:58:46 +0200

    Added UniformCost...Agent to Compile-Script, minor stuff in UniformCost...Agent

--- CPE480-Lab3-BaseCode0/compileSearchAgents.bat
+++ CPE480-Lab3-BaseCode0/compileSearchAgents.bat
@@ -2,4 +2,5 @@ javac -classpath BotEnvironment.jar;. .\Agents\DummySearchAgent.java
 javac -classpath BotEnvironment.jar;. .\Agents\UninformedSearchAgent.java 
 javac -classpath BotEnvironment.jar;. .\Agents\BreadthFirstSearchAgent.java 
 javac -classpath BotEnvironment.jar;. .\Agents\DepthFirstSearchAgent.java 
+javac -classpath BotEnvironment.jar;. .\Agents\UniformCostSearchAgent.java 
 pause
